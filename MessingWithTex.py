66f56deb5b6a0b3fab59eaf94f0f14aed884ddeb
Eli Terry <e.terry1135@gmail.com>
2023-02-06 23:23:32 -0500

    I am 90% certain I got these darn circuits to work
    Circuit.py contains a scene that renders the first circuit that is in our physics notes.
    
    Plus some other fun

--- MessingWithTex.py
+++ MessingWithTex.py
@@ -1,25 +1,35 @@
 from manim import *
-from manimlib.imports import *
-from circuitanimlib.circuit import *
-
+from CircuitanimButForCE.circuit import *
 
 class DrawCircuit(Scene):
   def construct(self):
     res = Resistor()
     cap = Capacitor()
     batt = Battery()
+    ind = Inductor()
 
     batt.rotate(PI/2)
-    cap.rotate(-PI/2)
-    cap.shift(RIGHT*3)
-    res.shift(2*LEFT + UP*3)
-    batt.shift(3*LEFT)
+    batt.shift(LEFT)
+    res.shift(UP)
+    ind.rotate(-PI/2)
+    ind.shift(RIGHT)
+    cap.shift(DOWN)
 
 
     circ = Circuit()
     circ.connect(batt.get_right(),res.get_left())
-    circ.connect(res.get_right(),cap.get_left(),pin_top=True)
-    circ.connect_right_to_left(cap.get_right(),batt.get_left())
+    circ.connect(res.get_right(),ind.get_left(), pin_top=True)
+    circ.connect(cap.get_right(),ind.get_right(), pin_top=True)
+    circ.connect(batt.get_left(),cap.get_left())
     circ.render()
+
+    t = Tex(r"suck it patrick I can even do \LaTeX \; if I want")
+    t.shift(UP*3)
     
-    self.play(Create(batt),Create(res),Create(cap),Create(circ),run_time=3)
+    resLabel = Text("Resistor")
+    resLabel.shift(UP*2)
+
+    
+    self.play(Create(batt),Create(res),Create(cap),Create(ind), Create(circ), run_time=3)
+    self.play(Create(resLabel), run_time=1)
+    self.play(Create(t), run_time=3)
