f2f995d60b69e51cfe8f5e0f7dca17f0c43c0af5
Patrick Ireland <patrick.jazp.ireland@gmail.com>
2023-02-08 14:20:08 -0500

    Struggling to make a proton

--- Mastropolo.py
+++ Mastropolo.py
@@ -2,16 +2,29 @@ from manim import *
 
 class CreateElectron(Scene):
     def construct(self):
-        electronName = Tex(r"$e^{-}$", color=RED, font_size=144)
+     
+        electronName = Tex(r"$e^{-}$", color=BLUE, font_size=144)
         circle1 = Circle().surround(electronName)
-        circle1.set_fill(PINK, opacity=0.2)
+        circle1.set_color(BLUE)
+        circle1.set_fill(BLUE, opacity=0.2)
+        
+
         electron = VGroup()
         electron.add(circle1,electronName)
 
-        coulombVal = Tex(r"$= 1.6 \times 10^{-19} \text{ Mastropolo's}$", font_size=144)
-        coulombVal.next_to(electron, RIGHT, buff=0.5)
-        #still needs to fit on screen
-
-
         self.play(Create(electron))
+        self.play(electron.animate.shift(LEFT*5))
+
+        coulombVal = Tex(r"$= -1.6 \times 10^{-19} \text{ Mastropolo's}$", font_size=144)
+        coulombVal.next_to(electron, RIGHT, buff=0.5)
+
         self.play(Create(coulombVal))
+        self.wait()
+
+        proton = VGroup()
+        protonName = Tex(r"$p^{+}$", color=RED)
+        circle2 = Circle().surround(protonName)
+        circle2.set_fill(PINK, opacity=0.2)
+        proton.add(protonName,circle2)
+
+        self.play(Transform(electron,proton))
