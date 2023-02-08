6204226b8344197fdc5da5bd5e07db2176ccfe1b
Patrick Ireland <patrick.jazp.ireland@gmail.com>
2023-02-08 14:58:39 -0500

    Broke things again

--- Mastropolo.py
+++ Mastropolo.py
@@ -6,7 +6,7 @@ class CreateElectron(Scene):
         electronName = Tex(r"$e^{-}$", color=BLUE, font_size=144)
         circle1 = Circle().surround(electronName)
         circle1.set_color(BLUE)
-        circle1.set_fill(BLUE, opacity=0.2)
+        circle1.set_fill(BLUE_B, opacity=0.2)
         
 
         electron = VGroup()
@@ -18,13 +18,12 @@ class CreateElectron(Scene):
         coulombVal = Tex(r"$= -1.6 \times 10^{-19} \text{ Mastropolo's}$", font_size=144)
         coulombVal.next_to(electron, RIGHT, buff=0.5)
 
-        self.play(Create(coulombVal))
+        self.play(Write(coulombVal))
         self.wait()
+        self.play(electron.animate.to_edge(UL), coulombVal.animate.to_edge(UL))
 
         proton = VGroup()
         protonName = Tex(r"$p^{+}$", color=RED)
         circle2 = Circle().surround(protonName)
         circle2.set_fill(PINK, opacity=0.2)
         proton.add(protonName,circle2)
-
-        self.play(Transform(electron,proton))
